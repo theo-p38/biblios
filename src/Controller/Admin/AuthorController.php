335878a0765de4a0f00520486ed94185ef0c2dca
theo-p38 <theo.patrac@yahoo.com>
2026-03-29 17:13:50 +0200

    Create Author form + admin controller

---
+++ src/Controller/Admin/AuthorController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/admin/author')]
+
+final class AuthorController extends AbstractController
+{
+    #[Route('', name: 'app_admin_author')]
+    public function index(): Response
+    {
+        return $this->render('admin/author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+}
